0a0455834cb99525890c45f2158fadbb2b0c1164
OgutuJoseph <onyango.joseph@yahoo.com>
2022-04-10 19:46:19 +0300

    Homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import './App.css';
-import Header from './Header/Header';
-import Home from './Home/Home'; 
+import "./App.css";
+import Header from "./Header/Header";
+import Home from "./Home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     // BEM
-    <div className='App'> 
-      <Header /> 
-      <Home />  
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path='/' exact>
+            <Header />
+            <Home />
+          </Route>
+          <Route path='/checkout'>
+            <Header /> 
+            <h1>I am Checkout</h1>
+          </Route>
+        </Switch>
     </div>
+    </Router>
   );
 }
 
