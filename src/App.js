d61eb99c1f27becca89784e75392de22b182f69a
OgutuJoseph <onyango.joseph@yahoo.com>
2022-04-24 13:10:28 +0300

    User Authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Header/Header';
 import Home from './Home/Home';
 import Checkout from './Checkout/Checkout';
 import Login from './Login/Login';
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
 
 function App() {
+
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // 'blank' square bracket below: it will only run once when the App component loads
+
+    auth.onAuthStateChanged(authUser => {
+      console.log('Authenticated User: ', authUser);
+
+      if(authUser) 
+      {
+        // logged in session exists
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        });
+      }
+      else 
+      {
+        // session logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        });
+      }
+    })
+
+  }, [])
+
   return (
     // BEM
     <Router>
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,18 +1,39 @@
 import React, { useState } from 'react';
 import './Login.css';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { auth } from '../firebase';
 
 function Login() {
+
+    const history = useHistory();
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const signIn = e => {
         e.preventDefault() // no refreshing in react
+
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .then(auth => {
+                history.push('/');
+            })
+            .catch(error => alert(error.message));
     };
 
     const register= e => {
         e.preventDefault() // no refreshing in react
+
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+                // successfully created a new user
+                console.log('userr:', auth);
+                if (auth) {
+                    history.push('/');
+                }
+            })
+            .catch(error => alert(error.message));
     };
 
     return (
